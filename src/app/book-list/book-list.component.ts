7e1027f8cbfad3d44e25275c831791135f3fe577
Your Name <you@example.com>
2018-01-29 23:01:56 +0100

    Service BookStore mit asynchronem Laden hinzugefügt

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,42 +1,38 @@
 import { Thumbnail } from './../shared/thumbnail';
 import { Book } from './../shared/book';
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { BookStoreService } from '../shared/book-store.service';
+import { OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'bm-book-list',
   templateUrl: './book-list.component.html',
   styles: []
 })
-export class BookListComponent implements OnInit {
+export class BookListComponent implements OnInit, OnDestroy {
+  private subscriptions: Array<Subscription> = new Array<Subscription>();
 
   books: Array<Book>;
   @Output() showDetailsEvent = new EventEmitter<Book>();
 
-  constructor() { }
+  constructor(private bookStoreService: BookStoreService) { }
 
   ngOnInit() {
-    this.books = [
-      new Book(
-        '9871234567',
-        'Angular die Referenz',
-        ['Jodhannes Hoppe', 'Kai Wiesinger', 'Else Sonstnochwer'],
-        new Date(2017, 3, 1),
-        'von den Grundlagen über praktische Beispiele zum Expertenwissen',
-        5,
-        [new Thumbnail('https://ng-buch.de/cover2.jpg', 'Buchcover')],
-        'Mit Angular wird ihre Anwendung gut...'
-      ),
-      new Book(
-        '978543210',
-        'Angular for Dummies',
-        ['Kyle Richmond', 'Alan Potter', 'Serena Wiliams'],
-        new Date(2016, 1, 1),
-        'Easy introduction in angular framework for everybody',
-        3,
-        [new Thumbnail('https://ng-buch.de/cover1.jpg', 'Buchcover')],
-        'This book gives an overview of the google angular framework v5 for hobby programmers...'
-      )
-    ];
+    const getAllBooksSubscription: Subscription =
+      this.bookStoreService.getAllBooks().subscribe((value: Array<Book>) => {
+        if (value) {
+          this.books = value;
+        }
+      });
+      this.subscriptions.push(getAllBooksSubscription);
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((value: Subscription, index: number, array: Subscription[]) => {
+      value.unsubscribe();
+    });
+    this.subscriptions = null;
   }
 
   showDetails(book: Book) {
---
+++ src/app/shared/book-store.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Book } from './book';
+import { Thumbnail } from './thumbnail';
+
+@Injectable()
+export class BookStoreService {
+
+  private booksValue: Array<Book> = [new Book(
+      '9871234567',
+      'Angular die Referenz',
+      ['Jodhannes Hoppe', 'Kai Wiesinger', 'Else Sonstnochwer'],
+      new Date(2017, 3, 1),
+      'von den Grundlagen über praktische Beispiele zum Expertenwissen',
+      5,
+      [new Thumbnail('https://ng-buch.de/cover2.jpg', 'Buchcover')],
+      'Mit Angular wird ihre Anwendung gut...'
+    ),
+    new Book(
+      '978543210',
+      'Angular for Dummies',
+      ['Kyle Richmond', 'Alan Potter', 'Serena Wiliams'],
+      new Date(2016, 1, 1),
+      'Easy introduction in angular framework for everybody',
+      3,
+      [new Thumbnail('https://ng-buch.de/cover1.jpg', 'Buchcover')],
+      'This book gives an overview of the google angular framework v5 for hobby programmers...'
+  )];
+
+  private books: BehaviorSubject<Array<Book>> = new BehaviorSubject<Array<Book>>(this.booksValue);
+
+  constructor() { }
+
+  getAllBooks(): Observable<Array<Book>> {
+    return this.books;
+  }
+}
